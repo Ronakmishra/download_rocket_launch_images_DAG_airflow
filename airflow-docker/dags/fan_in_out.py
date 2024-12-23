2ff434e2b7d29c11b073b6846feb00af892f2be5
Ronakmishra <ronak.mishra404@gmail.com>
2024-12-22 22:52:20 -0500

    added the fan_in_out files

--- airflow-docker/dags/fan_in_out.py
+++ airflow-docker/dags/fan_in_out.py
@@ -2,31 +2,36 @@ import airflow
 
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 
 
 
 # CHANGE --------------------------------------------- CHANGE
+ERP_CHANGE_DATE = airflow.utils.dates.days_ago(1)
 
-#SALES *****
+def _pick_erp_system(**context):
+    if context['execution-date'] <ERP_CHANGE_DATE:
+        return "fetch_sales_old"
+    else:
+        return "fetch_sales_new"
+
 def  _fetch_sales_old(**context):
-    print("Fetching the old sales data")
-    
+    print("fetching old sales data")  
     
 def  _fetch_sales_new(**context):
-    print("Fetching the new  sales data")
+    print("fetching new  sales data")
 
+def  _clean_sales_old(**context):
+    print("clean old sales data")
 
-ERP_CHANGE_DATE = airflow.utils.dates.days.ago(1)
+def  _clean_sales_new(**context):
+    print("clean new sales data")
 
-def _fetch_sales(**context):
-    if context['execution_date'] < ERP_CHANGE_DATE:
-        _fetch_sales_old(**context)
-    else:
-        _fetch_sales_new(**context)
-        
-        
-#WEATHER *****
+# def _fetch_sales(**context):
+#     if context['execution_date'] < ERP_CHANGE_DATE:
+#         _fetch_sales_old(**context)
+#     else:
+#         _fetch_sales_new(**context) 
 
 def  _fetch_weather_old(**context):
     print("Fetching the old weather data")
@@ -35,11 +40,13 @@ def  _fetch_weather_old(**context):
 def  _fetch_weather_new(**context):
     print("Fetching the new  weather data")
 
-def _fetch_weather(**context):
-    if context['execution_date'] < ERP_CHANGE_DATE:
-        _fetch_weather_old(**context)
-    else:
-        _fetch_weather_new(**context)    
+
+
+# def _fetch_weather(**context):
+#     if context['execution_date'] < ERP_CHANGE_DATE:
+#         _fetch_weather_old(**context)
+#     else:
+#         _fetch_weather_new(**context)    
         
         
         
@@ -52,19 +59,44 @@ with DAG(
     schedule_interval="@daily",
 ) as dag:
     start = DummyOperator(task_id="start")
+    
+# CHANGE --------------------------------------------- CHANGE
 
-    fetch_sales = DummyOperator(task_id="fetch_sales",python_callable=_fetch_sales)
-    clean_sales = DummyOperator(task_id="clean_sales")
+    pick_erp_system = BranchPythonOperator(
+        task_id ="pick_erp_system", python_callable = _pick_erp_system
+    )
+    
+    fetch_sales_old=PythonOperator(
+        task_id= "fetch_sales_old", python_callable = _fetch_sales_old 
+    )
+    clean_sales_old=PythonOperator(
+        task_id= "clean_sales_old", python_callable = _clean_sales_old
+    )
+    
+    fetch_sales_new=PythonOperator(
+        task_id= "fetch_sales_new", python_callable = _fetch_sales_new 
+    )
+    clean_sales_new=PythonOperator(
+        task_id= "clean_sales_new", python_callable = _clean_sales_new 
+    )
 
-    fetch_weather = DummyOperator(task_id="fetch_weather",python_callable=_fetch_weather)
-    clean_weather = DummyOperator(task_id="clean_weather")
+
+# CHANGE --------------------------------------------- CHANGE
+
+
+    # fetch_sales = DummyOperator(task_id="fetch_sales",python_callable=_fetch_sales)
+    # clean_sales = DummyOperator(task_id="clean_sales")
+
+    # fetch_weather = DummyOperator(task_id="fetch_weather",python_callable=_fetch_weather)
+    # clean_weather = DummyOperator(task_id="clean_weather")
 
     join_datasets = DummyOperator(task_id="join_datasets")
     train_model = DummyOperator(task_id="train_model")
     deploy_model = DummyOperator(task_id="deploy_model")
 
-    start >> [fetch_sales, fetch_weather]
-    fetch_sales >> clean_sales
-    fetch_weather >> clean_weather
-    [clean_sales, clean_weather] >> join_datasets
+    start >> [pick_erp_system]
+    pick_erp_system >> [fetch_sales_old,fetch_sales_new]
+    fetch_sales_old >> clean_sales_old
+    fetch_sales_new >> clean_sales_new
+    [clean_sales_old,clean_sales_new] >>join_datasets
     join_datasets >> train_model >> deploy_model
